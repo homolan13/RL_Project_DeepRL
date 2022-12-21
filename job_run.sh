b30431ead333286911e3d8ca10bdc47502f42856
homolan13 <93644446+homolan13@users.noreply.github.com>
2022-12-21 14:44:12 +0100

    Does not work on ubelix

--- job_run.sh
+++ job_run.sh
@@ -11,7 +11,7 @@
 
 # Runtime and memory
 #SBATCH --time=01:30:00
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=2G
 
 #SBATCH --cpus-per-task=8
 
@@ -20,7 +20,7 @@
 #SBATCH --gres=gpu:rtx2080ti:2
 
 # Install dependencies #
-singularity exec --nv docker://pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime pip install -U simglucose
+singularity exec --nv docker://pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime pip install -U simglucose # Gym will also be installed
 
 # Run script #
 singularity exec --nv docker://pytorch/pytorch:1.11.0-cuda11.3-cudnn8-runtime python simglucose_DDPG.py
---
+++ test_ubelix.py
@@ -0,0 +1,14 @@
+print(1)
+
+import gym
+import simglucose
+
+print(2)
+
+def main():
+    print(4)
+
+
+if __name__=='__main__':
+    print(3)
+    main()
