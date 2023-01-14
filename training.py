e0aece3c787f3bace0e3cd26c71922c49770920e
homolan13 <93644446+homolan13@users.noreply.github.com>
2023-01-14 16:31:21 +0100

    Fire evening

---
+++ training.py
@@ -0,0 +1,200 @@
+import numpy as np
+import torch as th
+import torch.nn.functional as F
+from copy import deepcopy
+from datetime import datetime
+import simglucose
+import gym
+from gym.wrappers import FlattenObservation
+from tqdm import tqdm
+
+# Define Replay Buffer
+class ReplayBuffer:
+    def __init__(self, state_dim, action_dim, device, max_size=500):
+        self.device = device
+        self.max_size = max_size
+        self.size = 0
+        self.ptr = 0
+        self.state_buffer = np.zeros((max_size, state_dim))
+        self.action_buffer = np.zeros((max_size, action_dim))
+        self.next_state_buffer = np.zeros((max_size, state_dim))
+        self.reward_buffer = np.zeros((max_size, 1))
+        self.done_buffer = np.zeros((max_size, 1))
+
+    def store(self, state, action, reward, next_state, done):
+        self.state_buffer[self.ptr] = state
+        self.action_buffer[self.ptr] = action
+        self.next_state_buffer[self.ptr] = next_state
+        self.reward_buffer[self.ptr] = reward
+        self.done_buffer[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+
+    def sample(self, batch_size):
+        idx = np.random.randint(0, self.size, size=batch_size)
+        return (
+            th.tensor(self.state_buffer[idx], dtype=th.float32).to(self.device),
+            th.tensor(self.action_buffer[idx], dtype=th.float32).to(self.device),
+            th.tensor(self.reward_buffer[idx], dtype=th.float32).to(self.device),
+            th.tensor(self.next_state_buffer[idx], dtype=th.float32).to(self.device),
+            th.tensor(self.done_buffer[idx], dtype=th.float32).to(self.device)
+        )
+
+# Define Actor and Critic networks
+class Actor(th.nn.Module): # state -> action
+    def __init__(self, state_dim, action_dim):
+        super(Actor, self).__init__()
+        self.l1 = th.nn.Linear(state_dim, 200)
+        self.l2 = th.nn.Linear(200, 200)
+        self.l3 = th.nn.Linear(200, 10)
+        self.l4 = th.nn.Linear(10, action_dim)
+
+    def forward(self, state):
+        a = F.relu(self.l1(state))
+        a = F.relu(self.l2(a))
+        a = F.relu(self.l3(a))
+        a = th.sigmoid(self.l4(a))
+        return 1.8 * a + 0.2 # scale to [0.2, 2.0]
+
+class Critic(th.nn.Module): # state + action -> Q(s,a) (Q-Network)
+    def __init__(self, state_dimension, action_dimension):
+        super(Critic, self).__init__()
+        self.l1 = th.nn.Linear(state_dimension + action_dimension, 200)
+        self.l2 = th.nn.Linear(200, 200)
+        self.l3 = th.nn.Linear(200, 10)
+        self.l4 = th.nn.Linear(10, 1)
+
+    def forward(self, state, action):
+        q = F.relu(self.l1(th.cat([state, action], 1)))
+        q = F.relu(self.l2(q))
+        q = F.relu(self.l3(q))
+        q = self.l4(q)
+        return q
+
+# Define DDPG Agent
+class DDPGAgent(object):
+    def __init__(self, env, device, discount=0.9, tau=0.01):
+        self.env = env
+        self.state_dim = env.observation_space.shape[0]
+        self.action_dim = env.action_space.shape[0]
+        self.device = device
+        self.discount = discount
+        self.tau = tau
+        # Actor and Actor target
+        self.actor = Actor(self.state_dim, self.action_dim).to(device)
+        self.actor_optimizer = th.optim.Adam(self.actor.parameters(), lr=1e-4)
+        self.actor_target = deepcopy(self.actor)
+        # Critic and Critic target
+        self.critic = Critic(self.state_dim, self.action_dim).to(device)
+        self.critic_optimizer = th.optim.Adam(self.critic.parameters(), lr=1e-4)
+        self.critic_target = deepcopy(self.critic)
+        # Replay buffer
+        self.replay_buffer = ReplayBuffer(self.state_dim, self.action_dim, self.device)
+
+    def soft_update(self):
+        for target_param, local_param in zip(self.actor_target.parameters(), self.actor.parameters()):
+            target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
+        for target_param, local_param in zip(self.critic_target.parameters(), self.critic.parameters()):
+            target_param.data.copy_(self.tau * local_param.data + (1.0 - self.tau) * target_param.data)
+
+    def train(self, batch_size=32, target_update_period=100, max_iter=1000, max_patience=100):
+        critic_training_loss = []
+        min_critic_loss = float('inf')
+        patience = max_patience
+        for it in range(max_iter):
+
+            # Sample one episode
+            state, _ = self.env.reset()
+            done = False
+            minutes_until_death = 0
+            while not done:
+                noise = np.random.normal(0, 0.3, (3,))
+                action = self.actor(th.tensor(state, dtype=th.float32).to(self.device)).detach().cpu().numpy() + noise
+                next_state, reward, done, _, _ = self.env.step(action)
+                self.replay_buffer.store(state, action, reward, next_state, done)
+                state = next_state
+                minutes_until_death += 3
+            
+            print(f'Patient died after {minutes_until_death} minutes')
+            # Sample replay buffer
+            state, action, reward, next_state, done = self.replay_buffer.sample(batch_size)
+            # Compute the target Q value
+            target_q = self.critic_target(next_state, self.actor_target(next_state))
+            target_q = reward + (done * self.discount * target_q).detach()
+            # Get current Q estimate
+            current_q = self.critic(state, action)
+            # Compute critic loss
+            critic_loss = F.mse_loss(current_q, target_q)
+            # Optimize the critic
+            self.critic_optimizer.zero_grad()
+            critic_loss.backward()
+            self.critic_optimizer.step()
+            # Compute actor loss
+            actor_loss = -self.critic(state, self.actor(state)).mean()
+            # Optimize the actor
+            self.actor_optimizer.zero_grad()
+            actor_loss.backward()
+            self.actor_optimizer.step()
+            # Save training loss   
+            if critic_loss < min_critic_loss:
+                min_critic_loss = critic_loss
+                patience = max_patience
+            critic_training_loss.append([critic_loss.item(), actor_loss.item()])
+            
+            # Update target networks
+            if it % target_update_period == 0:
+                self.soft_update()
+
+            if it % 5 == 0:
+                print(f'\nIteration: {it+1}, Critic loss: {critic_loss.item():.3f} (min: {min_critic_loss:.3f}), Patience left: {patience}\n')
+
+            # Convergence check
+            patience -= 1
+            if patience == 0:
+                print('Critic converged...')
+                break
+
+        return critic_training_loss
+
+# Define reward function based on paper
+def custom_reward(BG_history):
+    BG = BG_history[-1]
+    # BG: blood glucose level
+    # Hypoglycemia: BG < 70 mg/dL
+    if 30 <= BG and BG < 70:
+        return -1.5
+    # Normoglycemia: 70 mg/dL < BG < 180 mg/dL
+    elif 70 <= BG and BG <= 180:
+        return 0.5
+    # Hyperglycemia: BG > 180 mg/dL
+    elif 180 < BG and BG <= 300:
+        return -0.8
+    elif 300 < BG and BG <= 350:
+        return -1
+    # Other cases
+    else:
+        return -2
+
+history_length = 6
+
+gym.envs.register(
+    id='simglucose-bolus',
+    entry_point='simglucose.envs:T1DSimEnvBolus',
+    kwargs={'patient_name': ['average_adolescent'],
+        'history_length': history_length, 'reward_fun': custom_reward,
+        'enable_meal': True})
+
+env = gym.make('simglucose-bolus')
+env = FlattenObservation(env)
+
+device = th.device('cuda' if th.cuda.is_available() else 'cpu')
+
+print('Starting Training...')
+agent = DDPGAgent(env=env, device=device)
+critic_loss = agent.train()
+
+
+import matplotlib.pyplot as plt
+
+plt.plot([x[0] for x in critic_loss])
