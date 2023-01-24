c62ddbcc86d8e6dd35a54c4486282b33eed456df
homolan13 <93644446+homolan13@users.noreply.github.com>
2023-01-24 13:50:30 +0100

    New action space [0.6,1.4]

--- modules/Agent.py
+++ modules/Agent.py
@@ -53,7 +53,7 @@ class Actor(th.nn.Module): # state -> action
         a = F.relu(self.l2(a))
         a = F.relu(self.l3(a))
         a = th.sigmoid(self.l4(a))
-        return 1.8 * a + 0.2 # scale to [0.2, 2.0]
+        return 0.8 * a + 0.6 # scale to [0.2, 2.0]
 
 class Critic(th.nn.Module): # state + action -> Q(s,a) (Q-Network)
     def __init__(self, state_dimension, action_dimension):
@@ -153,7 +153,7 @@ class DDPGAgent(object):
                     start_state = state
                     start_time = info['time']
                     if add_noise: # Add noise when doing general training
-                        noise = np.random.normal(0, 0.3, (self.action_dim,))
+                        noise = np.random.normal(0, 0.1, (self.action_dim,))
                     else: # No noise when doing personalized training
                         noise = np.zeros(shape=(self.action_dim,))
                     bolus_action = self.select_action(start_state) + noise
@@ -218,7 +218,7 @@ class DDPGAgent(object):
 
         return training_loss
 
-    def general_training(self, batch_size=32, target_update_period=100, iter=[1000, 7000], max_patience=600, path='agent_state', filename='general_training_loss.json'):
+    def general_training(self, batch_size=32, target_update_period=100, iter=[200, 1000], max_patience=100, path='agent_state', filename='general_training_loss.json'):
         g_training_loss = self._train(batch_size=batch_size, target_update_period=target_update_period, iter=iter, max_patience=max_patience, add_noise=True, path=path)
         self.is_pretrained = True
 
@@ -230,7 +230,7 @@ class DDPGAgent(object):
 
         return g_training_loss
 
-    def personalized_training(self, batch_size=32, target_update_period=100, iter=[500, 4000], max_patience=350, path='agent_state_finetuned', filename='personalized_training_loss.json'):
+    def personalized_training(self, batch_size=32, target_update_period=100, iter=[100, 500], max_patience=50, path='agent_state_finetuned', filename='personalized_training_loss.json'):
         assert self.is_pretrained == True, 'Agent must be pretrained before finetuning'
 
         ft_training_loss = self._train(batch_size=batch_size, target_update_period=target_update_period, iter=iter, max_patience=max_patience, add_noise=False, path=path)
@@ -243,7 +243,7 @@ class DDPGAgent(object):
 
         return ft_training_loss
 
-    def evaluate_policy(self, max_iter=int(4*24*3600/3), render=False, print_output=True): # Max_iter is set to yield an episode for 4 days, given sample rate of 3 min
+    def evaluate_policy(self, max_iter=int(4*24*60/3), render=False, print_output=True): # Max_iter is set to yield an episode for 4 days, given sample rate of 3 min
         CGM_idx = int(self.state_dim/3 - 1)
         CHO_idx = int(2*self.state_dim/3 - 1)
 
@@ -257,8 +257,8 @@ class DDPGAgent(object):
         for t in range(max_iter):
             
             if print_output:
-                print(20*' ', end='\r')
-                print('Still alive', (t%4)*'.', end='\r')
+                print(40*' ', end='\r')
+                print('Still alive' + (t%4)*'.' + (4-(t%4))*' ' + f'({t}/{max_iter})', end='\r')
             
             if render:
                 self.env.render(mode='human')
